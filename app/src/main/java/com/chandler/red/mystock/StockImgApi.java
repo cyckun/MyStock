4799e8d136c7c1b14274e20233b2561e7ff74b05
caoyongchao <caoyongchao@bytedance.com>
2025-01-23 19:58:14 +0800

    enable us huge daily image replace with SH300
    Change-Id: I1f0f82e93df80442e61e143bc154716ee5c314c8

--- app/src/main/java/com/chandler/red/mystock/StockImgApi.java
+++ app/src/main/java/com/chandler/red/mystock/StockImgApi.java
@@ -5,9 +5,9 @@ public class StockImgApi {
     public static final String SZIMG = "http://image.sinajs.cn/newchart/min/sz399001.gif";
     public static final String CHUANGIMG = "http://image.sinajs.cn/newchart/min/sz399006.gif";
     public static final String SH50 = "http://image.sinajs.cn/newchart/min/sh000016.gif";
-    public static final String SH300 = "http://image.sinajs.cn/newchart/min/sh000300.gif";
+    public static final String SH300 = "http://124.222.162.36:5000/news/image/us_huge_daily";
     public static final String ZX = "http://image.sinajs.cn/newchart/min/sh000905.gif";
-    public static final String DQS = "http://124.222.162.36:5000/news/image/us_today";
+    public static final String DQS = "http://124.222.162.36:5000/news/image/us_huge_today";
     public static final String NSDQ = "http://image.sinajs.cn/newchart/usstock/min/ixic.gif";
     public static final String HK = "http://image.sinajs.cn/newchart/hk_stock/min/hsi.gif";
     public static final String DQSOY = "http://image.sinajs.cn/newchart/min/dqsoy.gif";
